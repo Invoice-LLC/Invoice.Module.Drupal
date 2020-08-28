0dc1cb716164d36e04c413ce56961fb90592418d
Kirill A Kuznetsov <kk@invoice.su>
2020-08-28 14:18:04 +0300

    Fix: RestClient

--- modules/commerce_payment_invoice/src/PluginForm/OffsiteRedirect/InvoiceSDK/RestClient.php
+++ modules/commerce_payment_invoice/src/PluginForm/OffsiteRedirect/InvoiceSDK/RestClient.php
@@ -42,6 +42,7 @@ class RestClient
         ]);
 
         curl_setopt($ch,CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch,CURLOPT_SSL_VERIFYPEER, false);
 
         $response = curl_exec($ch);
         curl_close($ch);
